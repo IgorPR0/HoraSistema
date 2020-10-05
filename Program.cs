0d45d3812048a0f716138098c1948d815fe8a635
IgorPR0 <igortecnologia2020@gmail.com>
2020-10-05 01:46:52 -0300

    Concluido Atividade Data e hora

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime Hora;
+
+            Hora = DateTime.Now;
+            Console.Write("Data e hora Recente: ");
+            Console.WriteLine($"{Hora}");
+            Console.WriteLine();
+
+            Console.WriteLine(" Okay !");
+            Console.ReadKey();
+        }
+    }
+}
